4c2f317ffd800a3609fa3609a17920fea0d983db
Rafa Centeno <rgcenteno@edu.xunta.gal>
2021-10-12 21:38:52 +0200

    Formularios finalizado

--- views/formulario.view.php
+++ views/formulario.view.php
@@ -9,22 +9,22 @@
 
 <div class="row">
     <?php
-    if(isset($data['post'])){
-    ?>
-    <div class="col-12">
-        <div class="card shadow mb-4">
-            <div
-                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Variables pasadas:</h6>                                    
-            </div>
-            <!-- Card Body -->
-            <div class="card-body">
-                <?php var_dump($data['post']); ?>
-                <?php var_dump($data['sanitized_post']); ?>
+    if (isset($data['post'])) {
+        ?>
+        <div class="col-12">
+            <div class="card shadow mb-4">
+                <div
+                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Variables pasadas:</h6>                                    
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <?php var_dump($data['post']); ?>
+                    <?php var_dump($data['sanitized_post']); ?>
+                </div>
             </div>
         </div>
-    </div>
-    <?php
+        <?php
     }
     ?>
     <div class="col-12">
@@ -36,19 +36,18 @@
             <!-- Card Body -->
             <div class="card-body">
                 <form action="./?sec=formulario" method="post">
-                <!--<form method="get">-->
+                    <!--<form method="get">-->
                     <input type="hidden" name="sec" value="formulario" />
                     <div class="mb-3">
                         <label for="labelNombre">Nombre</label>
-                        <input class="form-control" id="labelNombre" type="text" name="nombre" placeholder="Inserte su nombre" value="<?php echo isset($data[S_POST]['nombre']) ? $data[S_POST]['nombre'] : "";?>">
+                        <input class="form-control" id="labelNombre" type="text" name="nombre" placeholder="Inserte su nombre" value="<?php echo isset($data[S_POST]['nombre']) ? $data[S_POST]['nombre'] : ""; ?>">
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlInput1">Correo electrónico</label>
-                        <input class="form-control" id="exampleFormControlInput1" type="email" name="email" placeholder="name@example.com" value="<?php echo isset($data[S_POST]['email']) ? $data[S_POST]['email'] : "";?>">
-                        <?php
-                        if($data['formSent'] && is_null($data[S_POST]['email'])){ ?>
-                        <p class="text-danger">Ha insertado un email no válido.</p>
-                        <?php                        
+                        <input class="form-control" id="exampleFormControlInput1" type="email" name="email" placeholder="name@example.com" value="<?php echo isset($data[S_POST]['email']) ? $data[S_POST]['email'] : ""; ?>">
+                        <?php if ($data['formSent'] && is_null($data[S_POST]['email'])) { ?>
+                            <p class="text-danger">Ha insertado un email no válido.</p>
+                            <?php
                         }
                         ?>
                     </div>
@@ -74,28 +73,27 @@
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlSelect2">Ejemplo checkbox</label>  
-
-                            <div class="row">
+                        <div class="row">
                             <div class="col-sm">
-                            <div class="form-check">
-                                <input class="form-check-input" id="flexCheckDefault" type="checkbox" value="a" name="opcions[]" <?php echo in_array("a", $data[S_POST]['opcions']) !== FALSE ? "checked" : ''; ?>>
-                                <label class="form-check-label" for="flexCheckDefault">Default checkbox</label>
-                            </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" id="flexCheckDefault" type="checkbox" value="a" name="opcions[]" <?php echo in_array("a", $data[S_POST]['opcions']) !== FALSE ? "checked" : ''; ?>>
+                                    <label class="form-check-label" for="flexCheckDefault">Default checkbox</label>
+                                </div>
                             </div>
                             <div class="col-sm">
-                            <div class="form-check">
-                                <input class="form-check-input" id="flexCheckChecked" type="checkbox" value="b" checked" name="opcions[]" <?php echo in_array("b", $data[S_POST]['opcions']) !== FALSE ? "checked" : ''; ?>>
-                                <label class="form-check-label" for="flexCheckChecked">Checked checkbox</label>
+                                <div class="form-check">
+                                    <input class="form-check-input" id="flexCheckChecked" type="checkbox" value="b" checked" name="opcions[]" <?php echo in_array("b", $data[S_POST]['opcions']) !== FALSE ? "checked" : ''; ?>>
+                                    <label class="form-check-label" for="flexCheckChecked">Checked checkbox</label>
+                                </div>
                             </div>
-                            </div>
-                        <div class="col-sm">
-                            <div class="form-check">
-                                <input class="form-check-input" id="flexCheckDisabled" type="checkbox" value="c" disabled name="opcions[]" <?php echo in_array("c", $data[S_POST]['opcions']) !== FALSE ? "checked" : ''; ?>>
-                                <label class="form-check-label" for="flexCheckDisabled">Disabled checkbox</label>
+                            <div class="col-sm">
+                                <div class="form-check">
+                                    <input class="form-check-input" id="flexCheckDisabled" type="checkbox" value="c" disabled name="opcions[]" <?php echo in_array("c", $data[S_POST]['opcions']) !== FALSE ? "checked" : ''; ?>>
+                                    <label class="form-check-label" for="flexCheckDisabled">Disabled checkbox</label>
+                                </div>
                             </div>
                         </div>
-                            </div>
-                        </div>
+                    </div>
                     <div class="mb-3">
                         <label for="exampleFormControlTextarea1">Ejemplo textarea</label>
                         <textarea class="form-control" id="exampleFormControlTextarea1" name="textarea" rows="3"><?php echo isset($data['sanitized_post']['textarea']) ? $data['sanitized_post']['textarea'] : ''; ?></textarea>
